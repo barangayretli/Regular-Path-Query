7ee6141cd017b22c33fcbe4a48397b0390c24743
Baran <barangayretli@gmail.com>
2019-08-02 13:33:55 -0400

    CSR bug fix

--- graph.cpp
+++ graph.cpp
@@ -123,7 +123,7 @@ void productGraph::printArr(vector<pair<string,int>> resultArr)
 CSR::CSR(int n, int m) {
     this -> n = n;// vertex number
     this -> m = m;// neighbor number
-    indices = new int[n]; // dynamically create indices array
+    indices = new int[n+1]; // dynamically create indices array
     CSRmatrix = new int[m]; // dynamically create CSR matrix array
     inverted = new string[n]; // dtnamically create inverted array
 }
@@ -202,9 +202,11 @@ void CSR::buildIndexArr(productGraph p)
         currIndex += it->second.adjacentVertices.size();
         i++;
     }
+    indices[i] = currIndex;
+    i++;
     
     // for vertex that do not have outgoing edges
-    for(; i < n ; i++) {
+    for(; i < n + 1 ; i++) {
         indices[i] = currIndex;
     }
 }
