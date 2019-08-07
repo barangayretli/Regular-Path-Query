62a944414422d54099b3d03ec988c2434f67e00c
Baran Gayretli <barangayretli@sabanciuniv.edu>
2019-08-07 00:08:19 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main(){
     ifstream graphRead, automataFile;
     ///////////////////////
     graphRead.open("/home/bgayretl/datasets/yago2s/yago2s_full_shuffle_virtuoso_tem53.tsv");
-    automataFile.open("pvldb4.txt");
+    automataFile.open("pvldb5.txt");
     ///////////////////////
     vector<pair<string,int>> vertices_ProductGraph0;
     vector<string> vertices_CSR0;
