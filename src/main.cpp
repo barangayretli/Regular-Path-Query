d58d4e45a0455c106a0e10290477d3cbded804f2
Baran Gayretli <bgayretl@tem54.tembo-domain.cs.uwaterloo.ca>
2019-08-07 12:38:59 -0400

    Exit program if no argument is supplied

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@ int main(int argc, char *argv[]){
 
 	if(argc < 3) {
 		cout << "Supply graph file and automata file as arguments" << endl;
+		exit(0);
 	}
     
     ifstream graphRead, automataFile;
