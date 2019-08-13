fc1d46725522ad18314bcb06e6ab236d8e3fa429
Baran Gayretli <barangayretli@sabanciuniv.edu>
2019-08-13 11:46:27 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -163,7 +163,9 @@ int main(int argc, char *argv[]){
 
 void printExecutionTime(ClockTime start_time, ClockTime end_time)
 {
+    auto execution_time_ns = duration_cast<nanoseconds>(end_time - start_time).count();
     auto execution_time_ms = duration_cast<microseconds>(end_time - start_time).count();
+    auto execution_time_mls = duration_cast<milliseconds>(end_time-start_time).count();
     auto execution_time_sec = duration_cast<seconds>(end_time - start_time).count();
     auto execution_time_min = duration_cast<minutes>(end_time - start_time).count();
     auto execution_time_hour = duration_cast<hours>(end_time - start_time).count();
@@ -175,6 +177,10 @@ void printExecutionTime(ClockTime start_time, ClockTime end_time)
         cout << "" << execution_time_min % 60 << " Minutes, ";
     if(execution_time_sec > 0)
         cout << "" << execution_time_sec % 60 << " Seconds, ";
+    if(execution_time_sec > 0)
+        cout << "" << execution_time_mls << " MiliSeconds, ";
     if(execution_time_ms > 0)
         cout << "" << execution_time_ms % long(1E+3) << " MicroSeconds, ";
+    if(execution_time_ns > 0)
+        cout << "" << execution_time_ns % long(1E+6) << " NanoSeconds, \n";
 }
