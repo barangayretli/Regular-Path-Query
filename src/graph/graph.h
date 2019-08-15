63a3b158b47a22cd0528a590ea162ca07cb70010
Baran Gayretli <barangayretli@sabanciuniv.edu>
2019-08-15 16:07:24 -0400

    Update graph.h

--- src/graph/graph.h
+++ src/graph/graph.h
@@ -15,7 +15,6 @@ private:
         std::vector<std::pair<std::string,int>> adjacentVertices; // vector of adjacent vertices of a vertex
     };
     std::unordered_map<std::pair<std::string,int>,adjVert,boost::hash<std::pair<std::string,int>>> ProductMap; // starting vertex map
-    std::unordered_set<std::pair<std::string,int>,boost::hash<std::pair<std::string,int>>> neighborset;
     friend class CSR;
     
 public:
@@ -24,6 +23,7 @@ public:
     int neighborNumCSR=0;
     int uniqueNeighbor=0;
     unsigned long edgeNumber=0;
+    std::unordered_set<std::pair<std::string,int>,boost::hash<std::pair<std::string,int>>> neighborset;
 };
 class CSR{
     
